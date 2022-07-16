bb8a11cb8927f237634a3488abd111f76051a1db
Afamsv <afamsv1234@gmail.com>
2022-07-16 15:55:52 +0100

    Storing a number that is too big into a variable

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marking_code_as_unchecked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int val1 = 2147483647;
+            int val2;
+
+            unchecked
+            {
+                val2 = val1 + 1;
+            }
+            Console.WriteLine("Val1 is {0}", val1);
+           Console.WriteLine("Val2 is {0}", val2);
+        }
+    }
+}
